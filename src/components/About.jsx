1f13d99aad1e93db74cfcc60eea3662d8f6d629b
Katie Breuil <kcbreuil@gmail.com>
2020-08-13 15:11:39 -0400

    removed build

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,13 @@
-import React from "react";
-import "../styling/about.css";
+import React from 'react';
+import '../styling/about.css';
 
 const About = () => {
   return (
     <div>
       <div className="about-flex">
         <div className="about">
-          <h1>About Me</h1>
-          <p>
-            Born in the county of dade, I came to tech with a passion for
-            connecting people and an eye for clean design. I graduated from
-            Florida State University in 2011 with a B.A. in Creative Writing.
-            When I'm not coding, you can find me snuggling my two cats and
-            chihuahua, Tico, that I adopted from a Family Dollar parking lot,
-            getting stretchy on my yoga mat, or watching a Disney movie I've
-            already seen 347 times.
-          </p>
+          <p>About Me</p>
+          <p>Web Developer.</p>
         </div>
       </div>
     </div>
