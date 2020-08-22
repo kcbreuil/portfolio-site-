94e85f1a7978c3e930d348189733dfe0a66955ba
Katie Breuil <kcbreuil@gmail.com>
2020-08-22 16:27:02 -0400

    about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,14 @@ const About = () => {
       <div className="about-flex">
         <div className="about">
           <p>About Me</p>
-          <p>Web Developer.</p>
+          <p>
+            Hi! I'm Katie. I am a Miami-based web developer, designer and
+            educator. I graduated from Florida State University with a B.A. in
+            Creative Writing. I came to the tech world with a background in
+            events, marketing and communications in the art industry. When I'm
+            not talking code, I am cuddling with my pets, working out, or
+            sipping a delicious-iced-caffeinated beverage, in no specific order.
+          </p>
         </div>
       </div>
     </div>
