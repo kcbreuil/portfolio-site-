e33492f7e10d2c6bdad0ab8ba557911c2bad7428
kcbreuil <kcbreuil@gmail.com>
2021-05-19 11:47:21 -0400

    updates pt 1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,9 +13,9 @@ const Projects = () => {
           <h1 className="project-name">QuaranRead</h1>
           <video src={quaranred} width="420" height="340" controls></video>
           <p>An application created using ReactJS, Express, Node.js and Google books API integration.</p>
-          <a href="https://the-market2020.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
+          <a href="https://quaranread2020.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
           <br></br>
-          <a href="https://github.com/wyncode/C37_final_farmers_market" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/kcbreuil/quaranread" target="_blank" rel="noopener noreferrer">
             Source Code
           </a>
         </div>
@@ -23,9 +23,9 @@ const Projects = () => {
           <h1 className="project-name">The Market</h1>
           <video src={fm3} width="420" height="340" controls></video>
           <p>Concept was selected among ten peer pitches and was developed over a two-week sprint using ReactJS, MongoDB, Express and Node.js.</p>
-          <a href="https://quaranread2020.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
+          <a href="https://the-market2020.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
           <br></br>
-          <a href="https://github.com/wyncode/C37_midterm_katie_nestor_theo" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/kcbreuil/farmers_market" target="_blank" rel="noopener noreferrer">
             Source Code
           </a>
         </div>
@@ -41,9 +41,33 @@ const Projects = () => {
           <h1 className="project-name">Trivia Game</h1>
           <video src={netapp} width="420" height="340" controls></video>    
           <p>A trivia game created with ReactJS, MongoDB, Express, Node.JS, SendGrid and third-party API integration for NetApp and Google Cloud Next '20. </p>
-          {/* <a href="https://github.com/kcbreuil/chatroom-io">Source Code</a>
+          <a href="https://github.com/kcbreuil/chatroom-io">Source Code</a>
           <br></br>
-          <a href="https://github.com/kcbreuil/chatroom-io">Live Site</a> */}
+          <a href="https://github.com/kcbreuil/chatroom-io">Live Site</a>
+        </div>
+        <div className="project-div">
+          <h1 className="project-name">Pizza Pizza</h1>
+          {/* <video src={netapp} width="420" height="340" controls></video>     */}
+          <p>A Gatsby site developed with styled-components, Sanity headless CMS, and Netlify functions </p>
+          <a href="https://github.com/kcbreuil/pizza-pizza">Source Code</a>
+          <br></br>
+          <a href="https://pizza-pizzaa.netlify.app/">Live Site</a>
+        </div>
+        <div className="project-div">
+          <h1 className="project-name">Mortgage Calculator</h1>
+          {/* <video src={netapp} width="420" height="340" controls></video>     */}
+          <p>A simple web app built with good ol' Vanilla JS. Deployed with Dokku on my personal Digital Ocean server</p>
+          <a href="https://github.com/kcbreuil/mortgage-calculator">Source Code</a>
+          <br></br>
+          <a href="https://mortgage-calculator.kaitlynbreuil.com/">Live Site</a>
+        </div>
+        <div className="project-div">
+          <h1 className="project-name">Moodsic</h1>
+          {/* <video src={netapp} width="420" height="340" controls></video>     */}
+          <p>A fullstack web application built with React, Node.JS, Express and MongoDB</p>
+          <a href="https://github.com/kcbreuil/mortgage-calculator">Source Code</a>
+          <br></br>
+          <a href="https://mortgage-calculator.kaitlynbreuil.com/">Live Site</a>
         </div>
       </div>
     </div>
